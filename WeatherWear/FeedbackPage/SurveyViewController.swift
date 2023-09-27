ee7cf7be8e60ecf5fba6a76abc1cb020b4c47d81
chanho lee <leech3878@naver.com>
2023-09-27 19:27:48 +0900

    [feat] 피드백페이지 : 피드백페이지 버튼 기능 구현

---
+++ WeatherWear/FeedbackPage/SurveyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SurveyViewController.swift
+//  WeatherWear
+//
+//  Created by t2023-m079 on 2023/09/27.
+//
+
+import UIKit
+
+class SurveyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
