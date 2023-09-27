9f47e871230ac94d4f1c415788661a172ee96809
real-future <future.ver1@gmail.com>
2023-09-27 11:01:13 +0900

    [ui] 탭바 ui 구현

--- WeatherWear/TapbarController.swift
+++ WeatherWear/TapbarController.swift
@@ -15,17 +15,18 @@ class TapbarController: UITabBarController {
     }
 
     private func tabBarSetting() {
-        self.tabBar.backgroundColor = .red
+        self.tabBar.backgroundColor = .clear
         self.modalPresentationStyle = .fullScreen
         self.tabBar.unselectedItemTintColor = .systemGray
-        self.tabBar.tintColor = .black
+        self.tabBar.tintColor = .white
     }
 
     private func viewControllerSetting() {
         let vc1 = UINavigationController(rootViewController: ViewController())
         let vc2 = UINavigationController(rootViewController: WeeklyWearViewController())
-        vc1.tabBarItem = UITabBarItem(title: "오늘 뭐 입지?", image: nil, selectedImage: nil)
-        vc2.tabBarItem = UITabBarItem(title: "이번주 뭐 입지?", image: nil, selectedImage: nil)
+        vc1.tabBarItem = UITabBarItem(title: "Today", image: UIImage(named: "today"), selectedImage: nil)
+            vc2.tabBarItem = UITabBarItem(title: "Weekly", image: UIImage(named: "weekly"), selectedImage: nil)
+
 //        vc1.title = "홈"
 //        vc2.tabBarItem.title = "좋아요"
 
