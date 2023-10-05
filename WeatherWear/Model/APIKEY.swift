c365fde6d525e779339753d716e2e40d2e152108
JeongKiKi <jkh001301@gmail.com>
2023-10-05 10:33:38 +0900

    api

---
+++ WeatherWear/Model/APIKEY.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  WeatherWear
+//
+//  Created by 정기현 on 2023/10/05.
+//
+
+import Foundation
+
+let openweatherApiKey = "b2f2a11dd85166dcddc9018fda06b032"
+let NAVER_CLIENT_ID = "u0akneinx0"
+let NAVER_CLIENT_SECRET = "3lLpbAck4Hv3N4A4hY4FfrwDkADxSJh0umluy81v"
+let Reverse_NAVER_CLIENT_ID = "n6j4mr88zg"
+let Reverse_NAVER_CLIENT_SECRET = "zQuabAMDlqgHpGWMDcjldsQlL9Mp4xSxviCZhuh9"
